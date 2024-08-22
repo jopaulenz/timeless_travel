3975fe30af7baf45147f4e5753f07ece3b89f623
Josephine Paulenz <josephinepaulenz@gmail.com>
2024-08-22 14:31:38 +0200

    login

--- db/migrate/20240821124520_create_bookings.rb
+++ db/migrate/20240821124520_create_bookings.rb
@@ -1,10 +1,13 @@
 class CreateBookings < ActiveRecord::Migration[7.1]
   def change
     create_table :bookings do |t|
-      t.references :time_travel, null: false, foreign_key: true
       t.date :start_date
       t.date :end_date
-      t.string :user_name
+      t.references :user, null: false, foreign_key: true
+      t.references :time_travel, null: false, foreign_key: true
+      t.integer :price
+      t.string :period
+      t.boolean :status
 
       t.timestamps
     end
